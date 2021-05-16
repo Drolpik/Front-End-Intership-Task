ffd1994bccaeeb219ea3018623edc306f6ea2974
Drolpik <mrbarti98@gmail.com>
2021-05-16 19:44:55 +0200

    Finished task

--- front-end-internship/src/App.jsx
+++ front-end-internship/src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css';
-import EditIntern from './EditIntern';
-import InternList from './InternList';
+import './App.scss';
+import EditIntern from './components/EditIntern/EditIntern';
+import InternList from './components/InternList/InternList';
 import {Switch, Route} from 'react-router-dom';
+import logo from './assets/logo.svg';
 
 function App() {
   return (
     <div className="App">
+      <img className="Logo" src={logo} alt="logo" />
        <Switch>          
           <Route path="/interns/:id" exact>
             <EditIntern />
